b907fc7dcc3fde5dd0e35d403634a9251272ce4e
Maddy <maduvce26@gmail.com>
2020-07-08 19:23:43 +0530

    updated logout image

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule, LoginGuard, UserGuard } from './app-routing.module';
@@ -14,8 +15,6 @@ import { ForgotPasswordPage } from './forgot-password/forgot-password';
 import { PasscodePage } from './passcode/passcode';
 import { RegisterPage } from './register/register';
 import { ConfirmationPage } from './confirmation/confirmation';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
 import { AuthService } from './auth.service';
 
 @NgModule({
